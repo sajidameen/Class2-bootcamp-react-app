5989a4fd81514da2e239d940f0dcf8cdf9cfde3b
sajidameen <sajidmalik.0471@gmail.com>
2020-10-17 02:57:46 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div>
-      <h1>Salam to all and Welcome</h1>
+      <h1>Salam to all and Welcome to My testing React app</h1>
     </div>
   );
 }
